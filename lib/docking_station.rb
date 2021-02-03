78ed14eccf4f5a375d8c7e3ed9f302e25aa02e13
Sandy Pounoussamy <pounoussamy.sandy@gmail.com>
2021-02-03 16:03:44 +0000

    Added bikes attr_reader and its exception

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ require 'docking_station'
 describe DockingStation do
 
   describe '#release_bike' do
-    it "should respond_to release_bike" do
+    it 'should respond_to release_bike' do
       expect(subject).to respond_to :release_bike
     end
 
@@ -15,4 +15,18 @@ describe DockingStation do
       it { is_expected.to be_working }
     end
   end
+
+  describe '#dock_bike' do
+    # let(:bike) { instance_double(Bike) }
+    it 'should respond_to dock_bike with an argument' do
+      expect(subject).to respond_to(:dock_bike).with(1)
+    end
+  end
+
+  describe '#bikes' do
+    it 'should display the bikes instance variable' do
+      expect(subject.bikes).to be_an_instance_of Array
+    end
+  end
+
 end
