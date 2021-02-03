8299cdd1339506874973dcca454a285988e0a1ee
Adam Jonas <adamjonas25@gmail.com>
2021-02-03 15:03:34 +0000

    add tests for release bike and working?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,18 @@
-require './lib/docking_station.rb'
+require 'docking_station'
 
 describe DockingStation do
 
-  describe '#release_to' do
+  describe '#release_bike' do
     it "should respond_to release_bike" do
       expect(subject).to respond_to :release_bike
     end
-  end
 
+    context 'object released from release_bike' do
+      subject(:bike) { DockingStation.new.release_bike }
+
+      it { is_expected.to be_an_instance_of Bike }
+
+      it { is_expected.to be_working }
+    end
+  end
 end
